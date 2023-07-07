2124af5543bfd567cb6019e5f0e9a60b66e67b72
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-07 20:34:11 -0300

    feat: Removendo dependencias

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,9 @@
 import { app } from "./app";
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import { configSwagger } from "./plugins/swagger";
 import cors from "@fastify/cors";
-import { skinSwaggerRouter } from "./plugins/swagger/skins.swagger";
 
 app.register(cors, {
   origin: true,
 });
-
-app.register(fastifySwagger, configSwagger.swaggerOptions);
-app.register(fastifySwaggerUi, configSwagger.swaggerUiOptions);
-app.register(skinSwaggerRouter);
 
 app
   .listen({
