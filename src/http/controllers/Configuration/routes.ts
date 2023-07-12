6a9fc7a3c3a7339c194f68031d0c700dd67ee8ee
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-12 18:03:58 -0300

    feat: create findUser

--- src/http/controllers/Configuration/routes.ts
+++ src/http/controllers/Configuration/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import { createConfigurationController } from "./createConfigurationController";
 import { getManyConfigurationController } from "./getManyConfigurationController";
 import { getConfigurationController } from "./getConfigurationController";
+import { getUserConfigurationController } from "./getUserConfigurationController";
 
 export async function configurationRouter(app: FastifyInstance) {
   app.post("/v1/configuration", createConfigurationController);
   app.get("/v1/configuration", getManyConfigurationController);
   app.get("/v1/configuration/:id", getConfigurationController);
+  app.get("/v1/configuration/user/:owner_id", getUserConfigurationController);
 }
--- src/repositories/interface/IConfigurationRepository.ts
+++ src/repositories/interface/IConfigurationRepository.ts
@@ -1,6 +1,7 @@
 import { Configuration, Prisma } from "@prisma/client";
 
 export interface IConfigurationRepository {
+  findByUser(owner_id: string): Promise<Configuration>;
   findById(id: string): Promise<Configuration>;
   findByMany(): Promise<Configuration[]>;
   create(data: Prisma.ConfigurationCreateInput): Promise<Configuration>;
