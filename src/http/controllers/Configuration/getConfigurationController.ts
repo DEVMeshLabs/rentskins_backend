c9b80b57d834036a52bf75716e996d4357d9def2
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-12 17:51:21 -0300

    feat: Criando findById

---
+++ src/http/controllers/Configuration/getConfigurationController.ts
@@ -0,0 +1,22 @@
+import { ConfigurationNotExistError } from "@/useCases/errors/Configuration/ConfigurationNotExistError";
+import { makeGetConfigurationUseCase } from "@/useCases/factories/Configuration/makeGetConfiguration";
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function getConfigurationController(
+  req: FastifyRequest,
+  reply: FastifyReply
+) {
+  const { id } = req.params as { id: string };
+
+  try {
+    const makeGetConfiguration = makeGetConfigurationUseCase();
+    const findConfig = await makeGetConfiguration.execute(id);
+    return reply.status(200).send(findConfig);
+  } catch (error) {
+    if (error instanceof ConfigurationNotExistError) {
+      return reply.status(404).send({ error: error.message });
+    }
+
+    throw error;
+  }
+}
--- src/http/controllers/Configuration/routes.ts
+++ src/http/controllers/Configuration/routes.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { createConfigurationController } from "./createConfigurationController";
+import { getManyConfigurationController } from "./getManyConfigurationController";
+import { getConfigurationController } from "./getConfigurationController";
 
 export async function configurationRouter(app: FastifyInstance) {
   app.post("/v1/configuration", createConfigurationController);
+  app.get("/v1/configuration", getManyConfigurationController);
+  app.get("/v1/configuration/:id", getConfigurationController);
 }
