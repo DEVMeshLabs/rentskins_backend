bf1030d9a3fb80fefb908dc811f2d65eb6f77168
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-12 20:30:43 -0300

    feat(Skins):

--- src/repositories/interface/IWalletRepository.ts
+++ src/repositories/interface/IWalletRepository.ts
@@ -4,6 +4,7 @@ export interface IWalletRepository {
   findByMany(): Promise<Wallet[]>;
   findById(id: string): Promise<Wallet | null>;
   findByUser(owner_id: string): Promise<Wallet | null>;
+
   updateByUserValue(owner_id: string, valor: string): Promise<any>;
   create(data: Prisma.WalletCreateInput): Promise<Wallet>;
   deleteWallet(id: string): Promise<Wallet>;
