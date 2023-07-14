3cb21c2b32b3760d17f7c04abb9ccde0669e23fe
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-14 14:10:01 -0300

    feat: Adicionando Paginations

--- src/http/controllers/Skins/getInventoryUserController.ts
+++ src/http/controllers/Skins/getInventoryUserController.ts
@@ -1,3 +1,4 @@
+import { DataPagination } from "@/utils/dataPagination";
 import { FastifyRequest, FastifyReply } from "fastify";
 import SteamCommunity from "steamcommunity";
 const community = new SteamCommunity();
@@ -7,6 +8,11 @@ export function getInventoryController(
   reply: FastifyReply
 ) {
   const { id } = req.params as { id: string };
+  const { filterType, page, itemsPerPage } = req.body as {
+    filterType: string[];
+    page: number;
+    itemsPerPage: number;
+  };
   return community.getUserInventoryContents(
     id,
     730,
@@ -20,7 +26,28 @@ export function getInventoryController(
           err,
         });
       } else {
-        return reply.send(inventory);
+        const filterInventory = inventory.filter((item) => {
+          if (
+            item.tags[0].name === "Container" ||
+            item.tags[0].name === "Graffiti"
+          ) {
+            return false;
+          }
+          return true;
+        });
+        if (filterType.length === 0) {
+          return reply.send(
+            DataPagination.execute(page, itemsPerPage, filterInventory)
+          );
+        } else {
+          const filter = filterInventory.filter((item) => {
+            if (!filterType.includes(item.tags[0].name)) {
+              return false;
+            }
+            return true;
+          });
+          return reply.send(DataPagination.execute(page, itemsPerPage, filter));
+        }
       }
     }
   );
