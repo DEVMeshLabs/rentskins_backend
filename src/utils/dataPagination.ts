49a473ba555aa355c0ae34717bba0bae78f700e0
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-14 14:10:01 -0300

    feat: Adicionando Paginations

---
+++ src/utils/dataPagination.ts
@@ -0,0 +1,9 @@
+export class DataPagination {
+  public static execute(pages: number, itemsPerPage: number, array: Object[]) {
+    // Pages = 1 && itemsPerPage = 10 = 10
+    // Pages = 2 && itemsPerPage = 10 = 20
+    const pagination = pages * itemsPerPage;
+    const newArray = array.slice(pagination - itemsPerPage, pagination);
+    return newArray;
+  }
+}
