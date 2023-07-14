f0250b8e3b83c6cfba82c5650cb5d6f4c6a773b4
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-14 14:21:02 -0300

    feat: Validando campos do page

--- src/http/controllers/Skins/getInventoryUserController.ts
+++ src/http/controllers/Skins/getInventoryUserController.ts
@@ -1,6 +1,7 @@
 import { DataPagination } from "@/utils/dataPagination";
 import { FastifyRequest, FastifyReply } from "fastify";
 import SteamCommunity from "steamcommunity";
+import { z } from "zod";
 const community = new SteamCommunity();
 
 export function getInventoryController(
@@ -8,47 +9,62 @@ export function getInventoryController(
   reply: FastifyReply
 ) {
   const { id } = req.params as { id: string };
-  const { filterType, page, itemsPerPage } = req.body as {
-    filterType: string[];
-    page: number;
-    itemsPerPage: number;
-  };
-  return community.getUserInventoryContents(
-    id,
-    730,
-    2,
-    true,
-    "english",
-    (err: Error | null, inventory?: any) => {
-      if (err) {
-        return reply.send({
-          message: "Error",
-          err,
-        });
-      } else {
-        const filterInventory = inventory.filter((item) => {
-          if (
-            item.tags[0].name === "Container" ||
-            item.tags[0].name === "Graffiti"
-          ) {
-            return false;
-          }
-          return true;
-        });
-        if (filterType.length === 0) {
-          return reply.send(
-            DataPagination.execute(page, itemsPerPage, filterInventory)
-          );
+
+  const inventorySchema = z.object({
+    filterType: z.string().array(),
+    page: z.number(),
+    itemsPerPage: z.number(),
+  });
+
+  const { filterType, itemsPerPage, page } = inventorySchema.parse(req.body);
+
+  try {
+    return community.getUserInventoryContents(
+      id,
+      730,
+      2,
+      true,
+      "english",
+      (err: Error | null, inventory?: any) => {
+        if (err) {
+          return reply.send({
+            message: "Error",
+            err,
+          });
         } else {
-          const filter = filterInventory.filter((item) => {
-            if (!filterType.includes(item.tags[0].name)) {
+          const filterInventory = inventory.filter((item) => {
+            if (
+              item.tags[0].name === "Container" ||
+              item.tags[0].name === "Graffiti"
+            ) {
               return false;
             }
             return true;
           });
-          return reply.send(DataPagination.execute(page, itemsPerPage, filter));
+          if (filterType.length === 0) {
+            return reply.send(
+              DataPagination.execute(page, itemsPerPage, filterInventory)
+            );
+          } else {
+            const filter = filterInventory.filter((item) => {
+              if (!filterType.includes(item.tags[0].name)) {
+                return false;
+              }
+              return true;
+            });
+            return reply.send(
+              DataPagination.execute(page, itemsPerPage, filter)
+            );
+          }
         }
       }
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return reply
+        .status(400)
+        .send({ message: "Erro de validação", errors: error.errors });
     }
-  );
+    throw error;
+  }
 }
