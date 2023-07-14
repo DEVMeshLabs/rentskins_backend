399fefda3394d10c2c32a23118cf88fd0c48df93
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-14 17:03:49 -0300

    feat: Adicionando totalDeMax

--- src/utils/dataPagination.ts
+++ src/utils/dataPagination.ts
@@ -2,8 +2,13 @@ export class DataPagination {
   public static execute(pages: number, itemsPerPage: number, array: Object[]) {
     // Pages = 1 && itemsPerPage = 10 = 10
     // Pages = 2 && itemsPerPage = 10 = 20
+    const maxPages = Math.ceil(array.length / itemsPerPage);
     const pagination = pages * itemsPerPage;
     const newArray = array.slice(pagination - itemsPerPage, pagination);
-    return newArray;
+    return {
+      maxItems: array.length,
+      maxPages,
+      inventory: newArray,
+    };
   }
 }
