34263c47c5baa07bf1d256bb28759530ad7960d0
ItaloAraujodev <italoarauju2019@gmail.com>
2023-07-25 12:16:44 -0300

    feat: Validando getbyId de cart

--- src/useCases/Cart/getCartUseCase.ts
+++ src/useCases/Cart/getCartUseCase.ts
@@ -1,10 +1,16 @@
 import { ICartRepository } from "@/repositories/interface/ICartRepository";
 import { Cart } from "@prisma/client";
+import { CartNotExistError } from "../errors/Cart/CartNotExistError";
 
 export class GetCartUseCase {
   constructor(private cartRepository: ICartRepository) {}
   async execute(id: string): Promise<Cart> {
     const getCart = await this.cartRepository.findById(id);
+
+    if (!getCart) {
+      throw new CartNotExistError();
+    }
+
     return getCart;
   }
 }
