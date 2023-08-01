2ba2d710d1b8abba0414824142fcfca7af45935d
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-01 13:47:58 -0300

    feat: teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
+const { resolve } = require("webpack");
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: "vitest",
+        replacement: resolve(__dirname, "./src"),
+      },
+    ],
+  },
 });
