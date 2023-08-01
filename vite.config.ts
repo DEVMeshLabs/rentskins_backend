a01dd4d26979fc1bcb8f83a53ecece0bcd978cc1
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-01 13:17:32 -0300

    feat: novo test

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  resolve: {
-    alias: {
-      $lib: path.resolve(__dirname, "./src/lib"),
-    },
-  },
 });
