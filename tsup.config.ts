473d1cb9856dd5a4d72ef28a6653427a08b47b88
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-01 13:29:32 -0300

    feat: teste

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,8 @@ import type { Options } from "tsup";
 
 export const tsup: Options = {
   clean: true,
-  entryPoints: ["src", "!src/**/*.spec.*"],
+  entry: ['"src", "!src/**/*.spec.*"'],
+  entryPoints: ["src"],
   format: ["esm", "cjs"],
   legacyOutput: true,
   sourcemap: true,
