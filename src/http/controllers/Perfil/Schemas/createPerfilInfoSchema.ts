6e97f7a3d44cd82cc278ee096d7318e278d336f6
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-01 18:21:55 -0300

    feat: Adicionando steam_level

--- src/http/controllers/Perfil/Schemas/createPerfilInfoSchema.ts
+++ src/http/controllers/Perfil/Schemas/createPerfilInfoSchema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const createPerfilInfoSchema = z.object({
   owner_id: z.string(),
+  steam_level: z.string(),
   status_member: z.string().optional(),
   delivery_time: z.string().optional(),
   delivery_fee: z.string().optional(),
