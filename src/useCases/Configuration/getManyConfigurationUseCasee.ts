4dbdb1efa05330948bbc306f177472cba4b77596
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-01 13:59:03 -0300

    feat:

--- src/useCases/Configuration/getManyConfigurationUseCasee.ts
+++ src/useCases/Configuration/getManyConfigurationUseCasee.ts
@@ -1,39 +1,39 @@
-import { describe, expect, it, beforeEach } from "vitest";
-import { InMemoryConfiguration } from "@/repositories/in-memory/inMemoryConfigurationRepository";
-import { GetManyConfigurationUseCase } from "./getManyConfigurationUseCase";
+// import { describe, expect, it, beforeEach } from "vitest";
+// import { InMemoryConfiguration } from "@/repositories/in-memory/inMemoryConfigurationRepository";
+// import { GetManyConfigurationUseCase } from "./getManyConfigurationUseCase";
 
-let configRepository: InMemoryConfiguration;
-let sut: GetManyConfigurationUseCase;
+// let configRepository: InMemoryConfiguration;
+// let sut: GetManyConfigurationUseCase;
 
-describe("GetMany Configuration Use Case", () => {
-  beforeEach(() => {
-    configRepository = new InMemoryConfiguration();
-    sut = new GetManyConfigurationUseCase(configRepository);
-  });
+// describe("GetMany Configuration Use Case", () => {
+//   beforeEach(() => {
+//     configRepository = new InMemoryConfiguration();
+//     sut = new GetManyConfigurationUseCase(configRepository);
+//   });
 
-  it("Should be able to get many configuration", async () => {
-    await configRepository.create({
-      owner_id: "76561198195920183",
-      owner_email: "italovital21@gmail.com",
-      owner_name: "Italo Araujo",
-      url_sell: "/",
-      url_trade: "/",
-      steam_guard: false,
-      agreed_with_emails: false,
-      agreed_with_terms: false,
-    });
+//   it("Should be able to get many configuration", async () => {
+//     await configRepository.create({
+//       owner_id: "76561198195920183",
+//       owner_email: "italovital21@gmail.com",
+//       owner_name: "Italo Araujo",
+//       url_sell: "/",
+//       url_trade: "/",
+//       steam_guard: false,
+//       agreed_with_emails: false,
+//       agreed_with_terms: false,
+//     });
 
-    await configRepository.create({
-      owner_id: "76561198195920183",
-      owner_email: "italovital21@gmail.com",
-      owner_name: "Italo Araujo",
-      url_sell: "/",
-      url_trade: "/",
-      steam_guard: false,
-      agreed_with_emails: false,
-      agreed_with_terms: false,
-    });
-    const manyConfiguration = await sut.execute();
-    expect(manyConfiguration.length).toEqual(2);
-  });
-});
+//     await configRepository.create({
+//       owner_id: "76561198195920183",
+//       owner_email: "italovital21@gmail.com",
+//       owner_name: "Italo Araujo",
+//       url_sell: "/",
+//       url_trade: "/",
+//       steam_guard: false,
+//       agreed_with_emails: false,
+//       agreed_with_terms: false,
+//     });
+//     const manyConfiguration = await sut.execute();
+//     expect(manyConfiguration.length).toEqual(2);
+//   });
+// });
