2428ae504e3a6da8ba4cc249c77b4a772ec2d33c
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-01 13:57:20 -0300

    feat:

--- tsup.config.ts
+++
@@ -1,6 +0,0 @@
-import type { Options } from "tsup";
-
-export const tsup: Options = {
-  entry: ['"src", "!src/**/*.spec.*"'],
-  config: "tsconfig.json",
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
-const { resolve } = require("webpack");
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  resolve: {
-    alias: [
-      {
-        find: "vitest",
-        replacement: resolve(__dirname, "./src"),
-      },
-    ],
-  },
 });
