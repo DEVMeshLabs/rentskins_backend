f02405472511aba2aa1a0f7d64e6d830ff037f92
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-02 11:01:27 -0300

    refactor: refatorando verifyToken

--- src/http/middlewares/verifyJwt.ts
+++ src/http/middlewares/verifyJwt.ts
@@ -6,7 +6,10 @@ export async function verifyJwt(req: FastifyRequest, reply: FastifyReply) {
   const bearToken = req.headers.authorization;
 
   if (!bearToken) {
-    return reply.redirect(401, "/");
+    return reply.status(401).send({
+      error:
+        "Por favor, forneça um token de autorização válido na solicitação.",
+    });
   }
   const token = bearToken.split("Bearer ")[1];
 
@@ -15,6 +18,9 @@ export async function verifyJwt(req: FastifyRequest, reply: FastifyReply) {
 
     return decode;
   } catch (error) {
-    return reply.redirect(403, "/");
+    return reply.status(403).send({
+      error:
+        "O token de autorização fornecido é inválido ou expirou. Por favor, verifique suas credenciais e tente novamente.",
+    });
   }
 }
