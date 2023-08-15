150c6f9e8472dbf4ab29ea8914520878972d4462
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-15 10:44:06 -0300

    refactor: Refatorando o getUserPerfil

--- src/useCases/Perfil/getUserPerfilUseCase.ts
+++ src/useCases/Perfil/getUserPerfilUseCase.ts
@@ -1,32 +1,16 @@
 import { Perfil } from "@prisma/client";
 import { IPerfilRepository } from "@/repositories/interfaceRepository/IPerfilRepository";
 import { PerfilNotExistError } from "../@errors/Perfil/PerfilInfoNotExistError";
-import axios from "axios";
-import { env } from "process";
 
 export class GetUserPerfilUseCase {
   constructor(private perfilRepository: IPerfilRepository) {}
 
   async execute(owner_id: string): Promise<Perfil> {
-    const response = await axios.get(
-      `https://api.steampowered.com/IPlayerService/GetSteamLevel/v1/?key=${env.STEAM_KEY}&steamid=${owner_id}`
-    );
-    const steamLevel = response.data.response.player_level;
+    const findUser = await this.perfilRepository.findByUser(owner_id);
 
-    const walletUser = await this.perfilRepository.findByUser(owner_id);
-
-    if (!walletUser) {
+    if (!findUser) {
       throw new PerfilNotExistError();
     }
-
-    if (walletUser.steam_level !== steamLevel) {
-      const updateLevel = await this.perfilRepository.updateLevel(
-        walletUser.id,
-        steamLevel
-      );
-      return updateLevel;
-    }
-
-    return walletUser;
+    return findUser;
   }
 }
