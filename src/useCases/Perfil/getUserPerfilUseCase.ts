94a94f840dbde04268a32e6d758442adbdbe7589
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-18 13:19:24 -0300

    refactor: Adicionando o setTimeout no float

--- src/useCases/Perfil/getUserPerfilUseCase.ts
+++ src/useCases/Perfil/getUserPerfilUseCase.ts
@@ -1,16 +1,18 @@
-import { Perfil } from "@prisma/client";
 import { IPerfilRepository } from "@/repositories/interfaceRepository/IPerfilRepository";
 import { PerfilNotExistError } from "../@errors/Perfil/PerfilInfoNotExistError";
 
 export class GetUserPerfilUseCase {
   constructor(private perfilRepository: IPerfilRepository) {}
 
-  async execute(owner_id: string): Promise<Perfil> {
+  async execute(owner_id: string): Promise<any> {
     const findUser = await this.perfilRepository.findByUser(owner_id);
 
     if (!findUser) {
       throw new PerfilNotExistError();
     }
-    return findUser;
+
+    const { configurationId, ...perfilWithoutConfigId } = findUser;
+
+    return perfilWithoutConfigId;
   }
 }
