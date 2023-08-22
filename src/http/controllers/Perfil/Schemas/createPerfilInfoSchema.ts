397d8a8e8ad9d27583637088c65c52a87d545835
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-22 14:07:37 -0300

    feat: Adicionando mais skin e retirando a validação do perfil de steam_url

--- src/http/controllers/Perfil/Schemas/createPerfilInfoSchema.ts
+++ src/http/controllers/Perfil/Schemas/createPerfilInfoSchema.ts
@@ -5,13 +5,5 @@ export const createPerfilInfoSchema = z.object({
   owner_name: z.string(),
   owner_country: z.string().optional(),
   picture: z.string(),
-  steam_url: z
-    .string()
-    .refine(
-      (value) =>
-        /^https:\/\/steamcommunity\.com\/profiles\/\d{17}$/.test(value),
-      {
-        message: "Não é um link válido da Steam.",
-      }
-    ),
+  steam_url: z.string().url(),
 });
--- src/utils/seedSkin.ts
+++ src/utils/seedSkin.ts
@@ -92,4 +92,99 @@ export const seedSkin = [
       "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M%listingid%A%assetid%D14854042335630554542",
     skin_link_steam: "/",
   },
+
+  {
+    skin_image:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposLOzLhRlxfbGTj5X09q_goWYkuHxPYTEhGlQ5vp5i_PA54jKhF2zowdyZ2z1LYCTJgQ9NwmG-1G9xb3qhMS0vZ7Jm3UyvSAg7HuMyRW_hhhOaPsv26Lv8Ntyjw",
+    skin_name: "StatTrak™ Five-SeveN | Urban Hazard",
+    skin_category: "Pistol",
+    skin_weapon: "Five-SeveN",
+    skin_price: "1210",
+    skin_float: "0,22172",
+    seller_name: "Italo",
+    seller_id: "76561199205585878",
+    skin_color: "8650AC",
+    status: "Pending",
+    sale_type: "Caçadora",
+    status_float: "Nova de fabrica",
+    skin_link_game:
+      "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M%listingid%A%assetid%D17215054401195139801",
+    skin_link_steam: "/",
+  },
+
+  {
+    skin_image:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbaqKAxf0v73dDBH_t26kL-GluX2P77YjG5V18J9herKyoD8j1yg5UU9YmulII6cJABoMlvU-FLoxe7m0ZHq7Z3NyiZm7HRxt3iPlkDmgxpSLrs4ffL9gek",
+    skin_name: "Glock-18 | Gamma Doppler",
+    skin_category: "Pistol",
+    skin_weapon: "Glock-18",
+    skin_price: "85",
+    skin_float: "0,22172",
+    seller_name: "Italo",
+    seller_id: "76561199205585878",
+    skin_color: "D2D2D2",
+    status: "Pending",
+    sale_type: "Caçadora",
+    status_float: "Nova de fabrica",
+    skin_link_game:
+      "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M%listingid%A%assetid%D10097816831699739014",
+    skin_link_steam: "/",
+  },
+
+  {
+    skin_image:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgposbaqKAxf0v73dDBH_t26kL-GluX2P77YjG5V18J9herKyoD8j1yg5UU9YmulII6cJABoMlvU-FLoxe7m0ZHq7Z3NyiZm7HRxt3iPlkDmgxpSLrs4ffL9gek",
+    skin_name: "StatTrak™ USP-S | The Traitor",
+    skin_category: "Pistol",
+    skin_weapon: "USP-S",
+    skin_price: "1340",
+    skin_float: "0,32172",
+    seller_name: "Italo",
+    seller_id: "76561199205585878",
+    skin_color: "CF6A32",
+    status: "Pending",
+    sale_type: "Caçadora",
+    status_float: "Nova de fabrica",
+    skin_link_game:
+      "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M%listingid%A%assetid%D9685769765432850349",
+    skin_link_steam: "/",
+  },
+
+  {
+    skin_image:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpoor-mcjhjxszcdD4b0963mYKEmMj4OrzZgiUDvMEp07GX9tikjgfg_xdkMGimLYeTdVc3Yl2D_QO3werogJHqtJic1zI97SuSa_L0",
+    skin_name: "StatTrak™ Tec-9 | Bamboozle",
+    skin_category: "Pistol",
+    skin_weapon: "Tec-9",
+    skin_price: "1549",
+    skin_float: "0,32172",
+    seller_name: "Italo",
+    seller_id: "76561199205585878",
+    skin_color: "CF6A32",
+    status: "Pending",
+    sale_type: "Caçadora",
+    status_float: "Bem desgastada",
+    skin_link_game:
+      "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20M%listingid%A%assetid%D11999956581369992090",
+    skin_link_steam: "/",
+  },
+
+  {
+    skin_image:
+      "-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJegJL_9C3moS0kfv7IbrdqWdY781lxLiSpd_w3wKw-xJrZDihJYCRIwI4MF-BqAC7kLzu0cXt6pyYmic2vHI8pSGKyuyQkBc",
+    skin_name: "AK-47 | Wild Lotus",
+    skin_category: "Rifle",
+    skin_weapon: "AK-47",
+    skin_price: "342",
+    skin_float: "0,32172",
+    seller_name: "Italo",
+    seller_id: "76561199205585878",
+    skin_color: "D2D2D2",
+    status: "Pending",
+    sale_type: "Caçadora",
+    status_float: "Bem desgastada",
+    skin_link_game:
+      "steam://rungame/730/76561202255233023/+csgo_econ_action_preview%20S%owner_steamid%A%assetid%D477564924471018837",
+    skin_link_steam: "/",
+  },
 ];
