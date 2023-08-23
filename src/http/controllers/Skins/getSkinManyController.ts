52e436f862b8bd8f2ed06cfc00d9ae365abfcbe8
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-23 10:20:02 -0300

    feat: Fazendo paginação no getManySkin

--- src/http/controllers/Skins/getSkinManyController.ts
+++ src/http/controllers/Skins/getSkinManyController.ts
@@ -1,13 +1,20 @@
 import { makeGetSkinMany } from "@/useCases/@factories/Skin/makeGetSkinManyUseCase";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
 
 export async function getSkinManyController(
   req: FastifyRequest,
   reply: FastifyReply
 ) {
   try {
+    const searchGymsQuerySchema = z.object({
+      page: z.coerce.number().min(1).default(1),
+    });
+
+    const { page } = searchGymsQuerySchema.parse(req.query);
+
     const getSkinMany = makeGetSkinMany();
-    const response = await getSkinMany.execute();
+    const response = await getSkinMany.execute(page);
 
     return reply.status(200).send(response);
   } catch (error) {
--- src/useCases/Skin/getSkinManyUseCase.ts
+++ src/useCases/Skin/getSkinManyUseCase.ts
@@ -1,17 +1,11 @@
 import { ISkinsRepository } from "@/repositories/interfaceRepository/ISkinsRepository";
 import { Skin } from "@prisma/client";
-import { SkinNotExistError } from "../@errors/Skin/SkinNotExistsError";
 
 export class GetSkinManyUseCase {
   constructor(private skinRepository: ISkinsRepository) {}
 
-  async execute(): Promise<Skin[]> {
-    const skins = await this.skinRepository.findByMany();
-
-    if (skins.length <= 0) {
-      throw new SkinNotExistError();
-    }
-
+  async execute(page: number): Promise<Skin[]> {
+    const skins = await this.skinRepository.findByMany(page);
     return skins;
   }
 }
