2faf33839410813b6354dfe22418fc77e3ac323a
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-24 19:24:16 -0300

    feat:

--- prisma/unseed.ts
+++ prisma/unseed.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.notification.deleteMany();
+  await prisma.skinToCart.deleteMany();
   await prisma.skin.deleteMany();
 }
 
