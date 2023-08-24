80070f56dc568f0d4b7e123d6bef9687d2838777
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-24 17:43:54 -0300

    feat: Adicionando deleteMany na seed e resolvendo o getManySkin

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,10 @@ import { seedSkin } from "../src/utils/seedSkin";
 const prisma = new PrismaClient();
 
 async function main() {
+  if (await prisma.skin.findMany()) {
+    await prisma.skin.deleteMany();
+  }
+
   await prisma.skin.createMany({
     data: seedSkin,
   });
