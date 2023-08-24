d2772ed9c995adcada34d14e14c9ed78684f7297
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-24 17:53:28 -0300

    feat:

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,8 +4,6 @@ import { seedSkin } from "../src/utils/seedSkin";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.skin.deleteMany();
-
   await prisma.skin.createMany({
     data: seedSkin,
   });
