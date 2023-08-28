eab377ffce62935e3180b40a1ee8daf937f4f0c4
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-28 13:25:38 -0300

    feat: atualizando migration

---
+++ prisma/migrations/20230828162433_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `steam_guard` on the `Configuration` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Configuration" DROP COLUMN "steam_guard";
