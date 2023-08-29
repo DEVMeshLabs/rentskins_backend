af16338f8db77d7e331477f9985bdea393c36377
ItaloAraujodev <italoarauju2019@gmail.com>
2023-08-29 10:18:22 -0300

    feat: Criando rota getManyInventory

--- src/useCases/@factories/Transaction/makeCreateTransactionUseCase.ts
+++ src/useCases/@factories/Transaction/makeCreateTransactionUseCase.ts
@@ -1,14 +1,7 @@
-import { PrismaTransactionRepository } from "@/repositories/Prisma/prismaTransactionRepository";
 import { PagarMeProvider } from "@/useCases/Transaction/PagarMeProvider";
-import { CreateTransactionUseCase } from "@/useCases/Transaction/createTransactionUseCase";
 
 export function makeCreateTransactionUseCase() {
-  const prismaSkinRepository = new PrismaTransactionRepository();
-  const paymentProvider = new PagarMeProvider();
-  const createSkinUseCase = new CreateTransactionUseCase(
-    prismaSkinRepository,
-    paymentProvider
-  );
+  const createSkinUseCase = new PagarMeProvider();
 
   return createSkinUseCase;
 }
