0b5424292e4e56f1ef9750d502d4f9e40729bb37
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-01 14:13:49 -0300

    feat: Ao deletar uma skin, envia uma notificação

--- src/useCases/Skin/deleteSkinUseCase.ts
+++ src/useCases/Skin/deleteSkinUseCase.ts
@@ -1,10 +1,13 @@
 import { ISkinsRepository } from "@/repositories/interfaceRepository/ISkinsRepository";
-
 import { SkinNotExistError } from "../@errors/Skin/SkinNotExistsError";
 import { Skin } from "@prisma/client";
+import { INotificationRepository } from "@/repositories/interfaceRepository/INotificationRepository";
 
 export class DeleteSkinUseCase {
-  constructor(private skinRepository: ISkinsRepository) {}
+  constructor(
+    private skinRepository: ISkinsRepository,
+    private notificationRepository: INotificationRepository
+  ) {}
 
   async execute(id: string): Promise<Skin> {
     const skinId = await this.skinRepository.findById(id);
@@ -14,6 +17,16 @@ export class DeleteSkinUseCase {
     }
 
     const deleteSkin = await this.skinRepository.deleteSkin(id);
+
+    if (deleteSkin.deletedAt !== null) {
+      await this.notificationRepository.create({
+        owner_id: deleteSkin.seller_id,
+        owner_name: deleteSkin.seller_name,
+        description: "Skin deleted successfully",
+        skin_id: deleteSkin.id,
+      });
+    }
+
     return deleteSkin;
   }
 }
