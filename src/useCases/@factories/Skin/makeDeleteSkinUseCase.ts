1415cf2dc0b4f27e99682c19e2a3bb9046cd5a13
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-01 14:13:49 -0300

    feat: Ao deletar uma skin, envia uma notificação

--- src/useCases/@factories/Skin/makeDeleteSkinUseCase.ts
+++ src/useCases/@factories/Skin/makeDeleteSkinUseCase.ts
@@ -1,9 +1,14 @@
+import { PrismaNotificationRepository } from "@/repositories/Prisma/prismaNotificationRepository";
 import { PrismaSkinRepository } from "@/repositories/Prisma/prismaSkinsRepository";
 import { DeleteSkinUseCase } from "@/useCases/Skin/deleteSkinUseCase";
 
 export function makeDeleteSkinUseCase() {
   const prismaSkinRepository = new PrismaSkinRepository();
-  const getskinUseCase = new DeleteSkinUseCase(prismaSkinRepository);
+  const prismaNotificationRepository = new PrismaNotificationRepository();
+  const getskinUseCase = new DeleteSkinUseCase(
+    prismaSkinRepository,
+    prismaNotificationRepository
+  );
 
   return getskinUseCase;
 }
