fd5aaef74671a2b5a6950f12fc35f935b3ec23d4
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-06 11:48:28 -0300

    feat:

--- src/http/controllers/Skins/createSkinController.ts
+++ src/http/controllers/Skins/createSkinController.ts
@@ -59,9 +59,9 @@ export async function createSkinController(
       return reply.status(409).send({ error: error.message });
     } else if (error instanceof ZodError) {
       return reply.status(400).send({ error: error.message });
-    } else if (error instanceof Error) {
-      return reply.status(500).send({ error: error.message });
     }
+    console.log(error);
+    return reply.status(500).send({ error: error.message });
   }
 
   return reply.status(201).send();
