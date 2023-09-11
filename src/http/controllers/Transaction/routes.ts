e06e2394b580cfad12cadba30f6db254694a2341
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-11 11:12:04 -0300

    feat:

--- src/http/controllers/Transaction/routes.ts
+++ src/http/controllers/Transaction/routes.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { createTransactionController } from "./createTransactionController";
-import { verifyJwt } from "@/http/middlewares/verifyJwt";
 import { getRetriveTransactionController } from "./getRetriveTransactionController";
+import { verifyJwt } from "@/http/middlewares/verifyJwt";
 
 export async function transactionRouter(app: FastifyInstance) {
-  app.addHook("onRequest", verifyJwt);
-  app.post("/v1/transaction", createTransactionController);
+  app.post(
+    "/v1/transaction",
+    { onRequest: verifyJwt },
+    createTransactionController
+  );
   app.get("/v1/transaction/:id", getRetriveTransactionController);
 }
