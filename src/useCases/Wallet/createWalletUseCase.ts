5fd732d0f739605f2348774304b58cd9f0980021
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-14 14:02:00 -0300

    refactor: Refatorando createWallet

--- src/useCases/Wallet/createWalletUseCase.ts
+++ src/useCases/Wallet/createWalletUseCase.ts
@@ -16,29 +16,22 @@ export class CreateWalletUseCase {
     owner_id,
     owner_name,
   }: Prisma.WalletCreateInput): Promise<Wallet> {
-    const isAlreadyWallet = await this.walletRepository.findByUser(owner_id);
-    const isNotExist = await this.perfilRepository.findByUser(owner_id);
+    const [isAlreadyWallet, isNotExist] = await Promise.all([
+      this.walletRepository.findByUser(owner_id),
+      this.perfilRepository.findByUser(owner_id),
+    ]);
 
-    if (isAlreadyWallet) {
-      throw new WalletAlreadyExistError();
-    }
-
-    if (!isNotExist) {
-      throw new PerfilNotExistError();
-    }
+    if (isAlreadyWallet) throw new WalletAlreadyExistError();
+    if (!isNotExist) throw new PerfilNotExistError();
 
     const cartResponse = await this.cartRepository.create({
       buyer_id: owner_id,
     });
-
-    const response = await Promise.all([
-      await this.walletRepository.create({
-        owner_id,
-        owner_name,
-      }),
-      await this.perfilRepository.updateByCart(owner_id, cartResponse.id),
-    ]);
-
-    return response[0];
+    const walletResponse = await this.walletRepository.create({
+      owner_id,
+      owner_name,
+    });
+    await this.perfilRepository.updateByCart(owner_id, cartResponse.id);
+    return walletResponse;
   }
 }
