17ae1aaa220ceb1bbc9df7cc97df832f38b79b94
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-14 11:11:30 -0300

    feat: Validando conta se tem vacBan

--- src/useCases/Perfil/createPerfilUseCase.ts
+++ src/useCases/Perfil/createPerfilUseCase.ts
@@ -2,6 +2,7 @@ import { IPerfilRepository } from "@/repositories/interfaceRepository/IPerfilRep
 import { Perfil, Prisma } from "@prisma/client";
 import { PerfilAlreadyExistError } from "../@errors/Perfil/PerfilInfoAlreadyExistError";
 import { IConfigurationRepository } from "@/repositories/interfaceRepository/IConfigurationRepository";
+import { VerifyAccountVacBanError } from "../@errors/Perfil/VerifyAccountVacBanError";
 
 export class CreatePerfilUseCase {
   constructor(
@@ -15,14 +16,24 @@ export class CreatePerfilUseCase {
   ): Promise<Perfil> {
     const findIdUser = await this.perfilRepository.findByUser(data1.owner_id);
 
-    if (findIdUser) {
+    // Verificando se o user tem BanVAC na conta
+    const vacBan = await fetch(
+      `http://api.steampowered.com/ISteamUser/GetPlayerBans/v1?key=0B98876BF7EBF6720920F4F00CD20FA3&steamids=${data1.owner_id}`
+    ).then((response) => response.json());
+
+    if (vacBan.players[0].VACBanned) {
+      throw new VerifyAccountVacBanError();
+    } else if (findIdUser) {
       throw new PerfilAlreadyExistError();
     }
-    const result = await this.configurationRespository.create({ ...date });
+
+    const configuration = await this.configurationRespository.create({
+      ...date,
+    });
 
     const create = await this.perfilRepository.create({
       ...data1,
-      configurationId: result.id,
+      configurationId: configuration.id,
     });
 
     return create;
