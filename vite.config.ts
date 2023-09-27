07c54fae62914c17a2547a845d0f44952479ba2f
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-27 19:49:31 -0300

    feat:

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { configDefaults, defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    ...configDefaults.exclude,
-    exclude: ["./src/useCases/**/*.{spec}.{ts}"],
+    exclude: ["src/useCases/**/*.spec.ts"],
   },
 });
