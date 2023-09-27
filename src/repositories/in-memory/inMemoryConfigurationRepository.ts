cebe1bce1fe8aeddfd370bf2d3faa4fcb687081d
ItaloAraujodev <italoarauju2019@gmail.com>
2023-09-27 17:55:59 -0300

    feat: Workflow

--- src/repositories/in-memory/inMemoryConfigurationRepository.ts
+++ src/repositories/in-memory/inMemoryConfigurationRepository.ts
@@ -1,44 +1,19 @@
 import { Configuration, Prisma } from "@prisma/client";
 import { IConfigurationRepository } from "../interfaceRepository/IConfigurationRepository";
-import { randomUUID } from "node:crypto";
 
 export class InMemoryConfiguration implements IConfigurationRepository {
+  create(data: Prisma.ConfigurationCreateInput): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateById(
+    owner_id: string,
+    data: Prisma.ConfigurationUncheckedUpdateManyInput
+  ): Promise<Prisma.BatchPayload> {
+    throw new Error("Method not implemented.");
+  }
+
   public config: Configuration[] = [];
-
-  // async updateById(
-  //   id: string,
-  //   data: Prisma.ConfigurationUncheckedUpdateManyInput
-  // ) {
-  //   const findConfig = this.config.findIndex((item) => item.id === id);
-  //   const nova = { ...this.config[findConfig], ...data };
-
-  //   if (findConfig !== -1) {
-  //     this.config[findConfig] = nova;
-  //     this.config[findConfig].updatedAt = new Date();
-  //     return this.config[findConfig];
-  //   }
-  //   return null;
-  // }
-
-  // async create(data: Prisma.ConfigurationCreateInput) {
-  //   const createConfig = {
-  //     id: randomUUID(),
-  //     owner_name: data.owner_name,
-  //     owner_id: data.owner_id,
-  //     owner_email: data.owner_email,
-  //     url_trade: data.url_trade,
-  //     url_sell: data.url_sell,
-  //     agreed_with_emails: data.agreed_with_emails,
-  //     agreed_with_terms: data.agreed_with_terms,
-  //     steam_guard: data.steam_guard,
-  //     createdAt: new Date(),
-  //     updatedAt: null,
-  //     deletedAt: null,
-  //   };
-
-  //   this.config.push(createConfig);
-  //   return createConfig;
-  // }
 
   async findByUser(owner_id: string) {
     const configUser = this.config.find((conf) => conf.owner_id === owner_id);
