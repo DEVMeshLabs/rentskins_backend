b2ead9a05c1e13685b6d42993ee08159e4656f1c
ItaloAraujodev <italoarauju2019@gmail.com>
2023-10-13 11:26:08 -0300

    feat: Filtrando totas as skin que são diferente que null no findMany

--- src/useCases/Skin/getSkinManyUseCase.ts
+++ src/useCases/Skin/getSkinManyUseCase.ts
@@ -18,10 +18,14 @@ export class GetSkinManyUseCase {
 
     const totalPages = Math.ceil(totalSkinsFound / itemsPerPage);
 
-    const skins = await this.skinRepository.findByMany(
+    const SkinsAll = await this.skinRepository.findByMany(
       pageNumber,
       itemsPerPage
     );
+
+    const skins = SkinsAll.filter((skin) => {
+      return skin.status === null;
+    });
 
     return {
       totalPages,
