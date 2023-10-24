1649a4b796b7968635eeb02f2bbf8a27796ada12
ItaloAraujodev <italoarauju2019@gmail.com>
2023-10-24 19:01:36 -0300

    test: Criando novos testes para transaction

--- src/repositories/in-memory/inMemoryConfigurationRepository.ts
+++ src/repositories/in-memory/inMemoryConfigurationRepository.ts
@@ -1,19 +1,42 @@
 import { Configuration, Prisma } from "@prisma/client";
 import { IConfigurationRepository } from "../interfaceRepository/IConfigurationRepository";
+import { randomUUID } from "crypto";
 
 export class InMemoryConfiguration implements IConfigurationRepository {
-  create(data: Prisma.ConfigurationCreateInput): Promise<any> {
-    throw new Error("Method not implemented.");
+  public config: Configuration[] = [];
+
+  private notImplemented(): Promise<any> {
+    return Promise.resolve(null);
+  }
+
+  async create(data: Prisma.ConfigurationCreateInput): Promise<Configuration> {
+    const config = {
+      id: data.owner_id ?? randomUUID(),
+      owner_id: data.owner_id,
+      owner_name: data.owner_name,
+      owner_email: "",
+      owner_phone: "",
+      owner_cpf: "",
+      url_sell: "",
+      url_trade: "",
+      agreed_with_emails: false,
+      agreed_with_terms: false,
+      createdAt: new Date(),
+      updatedAt: null,
+      deletedAt: null,
+    };
+
+    this.config.push(config);
+
+    return config;
   }
 
   updateById(
     owner_id: string,
     data: Prisma.ConfigurationUncheckedUpdateManyInput
   ): Promise<Prisma.BatchPayload> {
-    throw new Error("Method not implemented.");
+    return this.notImplemented();
   }
-
-  public config: Configuration[] = [];
 
   async findByUser(owner_id: string) {
     const configUser = this.config.find((conf) => conf.owner_id === owner_id);
--- src/repositories/in-memory/inMemoryPerfilRepository.ts
+++ src/repositories/in-memory/inMemoryPerfilRepository.ts
@@ -1,87 +1,103 @@
-// import { Prisma } from "@prisma/client";
-// import { IPerfilRepository } from "../interfaceRepository/IPerfilRepository";
-// import { randomUUID } from "crypto";
+import { Perfil, Prisma } from "@prisma/client";
+import { IPerfilRepository } from "../interfaceRepository/IPerfilRepository";
+import { randomUUID } from "crypto";
 
-// export class inMemoryPerfilRepository implements IPerfilRepository {
-//   public perfil = [];
-//   private notImplemented(): Promise<any> {
-//     return Promise.resolve(null);
-//   }
+export class InMemoryPerfilRepository implements IPerfilRepository {
+  public perfil: Perfil[] = [];
+  private notImplemented(): Promise<any> {
+    return Promise.resolve(null);
+  }
 
-//   async create(data: Prisma.PerfilUncheckedCreateInput) {
-//     const createdPerfil = {
-//       id: data.id ?? randomUUID(),
-//       owner_id: data.owner_id,
-//       owner_name: data.owner_name,
-//       owner_type: "Usuario",
-//       owner_country: null,
-//       account_status: "Ativo",
-//       steam_level: null,
-//       delivery_time: 0,
-//       total_exchanges: 0,
-//       total_exchanges_completed: 0,
-//       reliability: "Sem informações",
-//       steam_created_date: null,
-//       picture: data.picture,
-//       steam_url: data.steam_url,
-//       createdAt: new Date(),
-//       updatedAt: null,
-//       deletedAt: null,
-//     };
+  async create(data: Prisma.PerfilUncheckedCreateInput) {
+    const createdPerfil = {
+      id: data.id ?? randomUUID(),
+      owner_id: data.owner_id,
+      owner_name: data.owner_name,
+      owner_type: "Usuario",
+      owner_country: null,
+      account_status: "Ativo",
+      steam_level: null,
+      delivery_time: "",
+      total_exchanges: 0,
+      total_exchanges_completed: 0,
+      reliability: "Sem informações",
+      steam_created_date: null,
+      picture: data.picture,
+      steam_url: data.steam_url,
+      configurationId: data.configurationId,
+      stripe_id: null,
+      cart_id: null,
+      createdAt: new Date(),
+      updatedAt: null,
+      deletedAt: null,
+    };
 
-//     this.perfil.push(createdPerfil);
-//     return createdPerfil;
-//   }
+    this.perfil.push(createdPerfil);
+    return createdPerfil;
+  }
 
-//   findByUser(owner_id: string) {
-//     return this.notImplemented();
-//   }
+  async findByUser(owner_id: string): Promise<Perfil | null> {
+    const getUser = this.perfil.find((item) => item.owner_id === owner_id);
+    return getUser;
+  }
 
-//   findById(id: string) {
-//     return this.notImplemented();
-//   }
+  findById(id: string) {
+    return this.notImplemented();
+  }
 
-//   findManyTypeUser(owner_type: string) {
-//     return this.notImplemented();
-//   }
+  findManyTypeUser(owner_type: string) {
+    return this.notImplemented();
+  }
 
-//   findByMany() {
-//     return this.notImplemented();
-//   }
+  findByMany() {
+    return this.notImplemented();
+  }
 
-//   findByStatus(id: string): Promise<any> {
-//     return this.notImplemented();
-//   }
+  findByStatus(id: string): Promise<any> {
+    return this.notImplemented();
+  }
 
-//   findByUserNotDeleteAt(owner_id: string) {
-//     return this.notImplemented();
-//   }
+  findByUserNotDeleteAt(owner_id: string) {
+    return this.notImplemented();
+  }
 
-//   updateById(id: string, data: Prisma.PerfilUncheckedCreateInput) {
-//     return this.notImplemented();
-//   }
+  updateById(id: string, data: Prisma.PerfilUncheckedCreateInput) {
+    return this.notImplemented();
+  }
 
-//   updateByCart(owner_id: string, cart: string): Promise<any> {
-//     return this.notImplemented();
-//   }
+  updateByCart(owner_id: string, cart: string): Promise<any> {
+    return this.notImplemented();
+  }
 
-//   updateLevel(id: string, steam_level: number) {
-//     return this.notImplemented();
-//   }
+  updateLevel(id: string, steam_level: number) {
+    return this.notImplemented();
+  }
 
-//   updateByUser(owner_id: string, data: any) {
-//     return this.notImplemented();
-//   }
+  // updateByUser
+  async updateByUser(owner_id: string, data: any) {
+    const userProfileIndex = this.perfil.findIndex(
+      (item) => item.owner_id === owner_id
+    );
 
-//   updateByIdUser(id: string, data: any): Promise<any> {
-//     return this.notImplemented();
-//   }
+    if (userProfileIndex !== -1) {
+      this.perfil[userProfileIndex] = {
+        ...this.perfil[userProfileIndex],
+        ...data,
+      };
+    }
 
-//   deletePerfil(id: string, force: string) {
-//     return this.notImplemented();
-//   }
+    return this.perfil[userProfileIndex];
+  }
 
-//   deletePerfilBanco(id: string) {
-//     return this.notImplemented();
-//   }
-// }
+  updateByIdUser(id: string, data: any): Promise<any> {
+    return this.notImplemented();
+  }
+
+  deletePerfil(id: string, force: string) {
+    return this.notImplemented();
+  }
+
+  deletePerfilBanco(id: string) {
+    return this.notImplemented();
+  }
+}
