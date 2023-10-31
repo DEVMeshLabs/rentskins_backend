6c49e1dcc78910f12fb077df6d578e08067584ba
ItaloAraujodev <italoarauju2019@gmail.com>
2023-10-31 20:04:56 -0300

    refactor: Test: refatorando DockerFile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,41 @@
+# Use the official Node.js image from Docker Hub
 FROM node:latest
-WORKDIR /src
-COPY package*.json .
-COPY . .
+
+WORKDIR /app
+
+# Copie o arquivo de configuração do projeto e instale as dependências
+COPY package.json ./
+COPY package-lock.json ./
+COPY tsconfig.json ./
+COPY ./prisma ./prisma
+
 RUN npm ci
+
+# Copie os arquivos do aplicativo
+COPY . .
+
+# Execute o comando de construção do aplicativo
 RUN npm run build
-RUN npx prisma migrate deploy
+
+# Exponha a porta que seu aplicativo está ouvindo
 EXPOSE 3333
-CMD [ npm, run start]
+
+# Comando para iniciar o aplicativo
+CMD [ "npm", "build" ]
+
+# FROM node:latest
+
+# WORKDIR /app
+
+# COPY package*.json ./
+
+# COPY . .
+
+# RUN npm ci &&
+# RUN npm run build
+# RUN npx prisma migrate deploy
+
+# EXPOSE 3333
+
+# CMD [ "npm", "run", "build"]
+
--- src/useCases/Perfil/createPerfilUseCase.ts
+++ src/useCases/Perfil/createPerfilUseCase.ts
@@ -1,46 +1,51 @@
 import { IPerfilRepository } from "@/repositories/interfaceRepository/IPerfilRepository";
-import { Perfil, Prisma } from "@prisma/client";
-import { PerfilAlreadyExistError } from "../@errors/Perfil/PerfilInfoAlreadyExistError";
 import { IConfigurationRepository } from "@/repositories/interfaceRepository/IConfigurationRepository";
+import { Prisma, Perfil } from "@prisma/client";
+import fetch from "node-fetch";
+import { PerfilAlreadyExistError } from "../@errors/Perfil/PerfilInfoAlreadyExistError";
 import { VerifyAccountVacBanError } from "../@errors/Perfil/VerifyAccountVacBanError";
 
 export class CreatePerfilUseCase {
   constructor(
     private perfilRepository: IPerfilRepository,
-    private configurationRespository: IConfigurationRepository
+    private configurationRepository: IConfigurationRepository
   ) {}
 
   async execute(
-    configurationDate: Prisma.ConfigurationCreateInput,
-    perfilDate: Prisma.PerfilUncheckedCreateInput
+    configurationData: Prisma.ConfigurationCreateInput,
+    perfilData: Prisma.PerfilUncheckedCreateInput
   ): Promise<Perfil> {
-    const [foundUserProfile, foundUserProfileDeleAt] = await Promise.all([
-      this.perfilRepository.findByUser(perfilDate.owner_id),
-      this.perfilRepository.findByUserNotDeleteAt(perfilDate.owner_id),
+    const [existingProfile, existingProfileNotDeleted] = await Promise.all([
+      this.perfilRepository.findByUser(perfilData.owner_id),
+      this.perfilRepository.findByUserNotDeleteAt(perfilData.owner_id),
     ]);
 
-    const isVacBan = await fetch(
-      `http://api.steampowered.com/ISteamUser/GetPlayerBans/v1?key=0B98876BF7EBF6720920F4F00CD20FA3&steamids=${perfilDate.owner_id}`
-    ).then((response) => response.json());
+    const steamApiUrl = `http://api.steampowered.com/ISteamUser/GetPlayerBans/v1?key=0B98876BF7EBF6720920F4F00CD20FA3&steamids=${perfilData.owner_id}`;
+    const isVacBan = await fetch(steamApiUrl).then((response) =>
+      response.json()
+    );
 
     if (isVacBan.players[0].VACBanned) {
       throw new VerifyAccountVacBanError();
-    } else if (foundUserProfile) {
+    } else if (existingProfile) {
       throw new PerfilAlreadyExistError();
     }
 
-    if (foundUserProfileDeleAt && foundUserProfileDeleAt.deletedAt !== null) {
-      return this.perfilRepository.updateByIdUser(perfilDate.owner_id, {
+    if (
+      existingProfileNotDeleted &&
+      existingProfileNotDeleted.deletedAt !== null
+    ) {
+      return this.perfilRepository.updateByIdUser(perfilData.owner_id, {
         deletedAt: null,
       });
     }
 
-    const createdConfiguration = await this.configurationRespository.create({
-      ...configurationDate,
+    const createdConfiguration = await this.configurationRepository.create({
+      ...configurationData,
     });
 
     const createdPerfil = await this.perfilRepository.create({
-      ...perfilDate,
+      ...perfilData,
       configurationId: createdConfiguration.id,
     });
 
