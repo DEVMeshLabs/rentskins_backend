fd26655539022759b2520e0df59f6a1f941d1e87
ItaloAraujodev <italoarauju2019@gmail.com>
2023-11-03 18:16:08 -0300

    fix: Resolvendo erro do inventario privado

---
+++ src/useCases/@factories/Transaction/makeProcessTransaction.ts
@@ -0,0 +1,22 @@
+import { PrismaNotificationRepository } from "@/repositories/Prisma/prismaNotificationRepository";
+import { PrismaPerfilRepository } from "@/repositories/Prisma/prismaPerfilRepository";
+import { PrismaSkinRepository } from "@/repositories/Prisma/prismaSkinsRepository";
+import { PrismaTransactionRepository } from "@/repositories/Prisma/prismaTransactionRepository";
+import { PrismaWalletRepository } from "@/repositories/Prisma/prismaWalletRepository";
+import { ProcessTransaction } from "@/utils/processTransaction";
+
+export function makeProcessTransaction() {
+  const transactionRepository = new PrismaTransactionRepository();
+  const walletRepository = new PrismaWalletRepository();
+  const perfilRepository = new PrismaPerfilRepository();
+  const skinRepository = new PrismaSkinRepository();
+  const notificationsRepository = new PrismaNotificationRepository();
+  const proTransaction = new ProcessTransaction(
+    transactionRepository,
+    perfilRepository,
+    skinRepository,
+    walletRepository,
+    notificationsRepository
+  );
+  return proTransaction;
+}
