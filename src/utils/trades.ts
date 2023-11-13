0c898aba5d0a07e31c001c85fd346e0c05434983
ItaloAraujodev <italoarauju2019@gmail.com>
2023-11-13 17:48:19 -0300

    refactor: melhorando a logica do tradeHistory e removendo o campo media_price

--- src/utils/trades.ts
+++ src/utils/trades.ts
@@ -1,38 +1,54 @@
 import axios from "axios";
 
 export class Trades {
-  constructor() {}
-
-  static async getTradeHistory() {
-    const key = "C3B106395E5E2FCD39B30DF5E85C28E0";
-
-    const trades = axios
-      .get(
+  static async getTradeHistory(key: string) {
+    try {
+      const response = await axios.get(
         `https://api.steampowered.com/IEconService/GetTradeHistory/v1/?key=${key}&max_trades=50&get_descriptions=false&language=EN&include_failed=true&include_total=true`
-      )
-      .then((res) => res.data)
-      .catch((err) => err.message);
-
-    return trades;
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error.message);
+      throw error.message;
+    }
   }
 
-  static async filterTradeHistory(steamId: string, assetId: string) {
-    const trades = await this.getTradeHistory();
+  static async filterTradeHistory(): Promise<boolean | string> {
+    const steamidOther = "76561198862407248";
+    const assetId = "34489117389";
+    const key = "C3B106395E5E2FCD39B30DF5E85C28E0";
+    let isVerifyStatus: true | false = false;
 
-    if (trades.length > 0) {
-      const tradesFiltered = trades.response.trades.filter((trade) => {
-        return trade.steamid_other === steamId;
-      });
+    try {
+      const trades = await this.getTradeHistory(key);
 
-      const assets = tradesFiltered.flatMap((trade) => {
-        return trade.assets_received.filter((asset) => {
-          console.log(asset.assetid);
-          return asset.assetid === assetId;
+      if (trades.response.trades.length > 0) {
+        const tradesFiltered = trades.response.trades.filter(
+          (trade) => trade.steamid_other === steamidOther
+        );
+
+        const assets = tradesFiltered.flatMap((trade) => {
+          if (trade.status === 3) {
+            isVerifyStatus = true;
+          }
+
+          return trade.assets_received.filter(
+            (asset) => asset.assetid === assetId
+          );
         });
-      });
-      return assets;
+
+        const isAlreadyExistAsset = assets.some(
+          (item) => item.assetid === assetId && isVerifyStatus
+        );
+
+        return isAlreadyExistAsset;
+      }
+
+      console.log("No trades found");
+      return "No trades found";
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-    console.log("No trades found");
-    return "No trades found";
   }
 }
