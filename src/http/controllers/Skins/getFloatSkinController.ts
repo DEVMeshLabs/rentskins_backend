60802f4ef007c26dce0580aab51703545c4dcd49
ItaloAraujodev <italoarauju2019@gmail.com>
2024-01-23 18:07:01 -0300

    feat: criando alguns cenarios de erros

--- src/http/controllers/Skins/getFloatSkinController.ts
+++ src/http/controllers/Skins/getFloatSkinController.ts
@@ -1,12 +1,12 @@
 import { makeGetSkinFloatUseCase } from "@/useCases/@factories/Skin/makeGetSkinFloatUseCase";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { getFloatSchema } from "./Schemas/getFloatSchema";
 
 export async function getFloatSkinController(
   req: FastifyRequest,
   reply: FastifyReply
 ) {
-  const { url } = req.body as any;
-  console.log(url);
+  const { url } = getFloatSchema.parse(req.body);
 
   try {
     const getSkinFloat = makeGetSkinFloatUseCase();
@@ -14,6 +14,13 @@ export async function getFloatSkinController(
 
     return reply.status(200).send(response);
   } catch (error) {
-    console.log(error);
+    const errorMappings = {
+      SkinFloatUnauthorizedError: 401,
+      SkinFloatNoCreditsError: 402,
+      SkinFloatInvalidParametersLinkError: 406,
+      SkinFloatCantGetFloatError: 417,
+    };
+    const status = errorMappings[error.constructor.name] || 500;
+    return reply.status(status).send({ error: error.message });
   }
 }
