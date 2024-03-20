343d9e0b24497b4cb98e3f54611720ba9fa31226
ItaloAraujodev <italoarauju2019@gmail.com>
2024-03-19 21:07:45 -0300

    refactor: rental transaction schema and related code

--- src/useCases/@factories/RentalTransaction/makeCreateRentalTransaction.ts
+++ src/useCases/@factories/RentalTransaction/makeCreateRentalTransaction.ts
@@ -1,8 +1,8 @@
-import { PrismaConfigurationRepository } from "@/repositories/Prisma/prismaConfigurationRepository";
 import { PrismaNotificationRepository } from "@/repositories/Prisma/prismaNotificationRepository";
 import { PrismaPerfilRepository } from "@/repositories/Prisma/prismaPerfilRepository";
 import { PrismaRentalTransactionRepository } from "@/repositories/Prisma/prismaRentalTransactionRepository";
 import { PrismaSkinRepository } from "@/repositories/Prisma/prismaSkinsRepository";
+import { PrismaTransactionHistoryRepository } from "@/repositories/Prisma/prismaTransactionHistory";
 import { PrismaWalletRepository } from "@/repositories/Prisma/prismaWalletRepository";
 import { CreateRentalTransactionUseCase } from "@/useCases/RentalTransaction/createRentalTransactionUseCase";
 
@@ -13,16 +13,17 @@ export function makeCreateRentalTransactionUseCase() {
   const prismaSkinRepository = new PrismaSkinRepository();
   const prismaPerfilRepository = new PrismaPerfilRepository();
   const prismaWalletRepository = new PrismaWalletRepository();
+  const prismaTransactionHistoryRepository =
+    new PrismaTransactionHistoryRepository();
   const prismaNotificationRepository = new PrismaNotificationRepository();
-  const prismaConfigurationRepository = new PrismaConfigurationRepository();
 
   const createRentalTransaction = new CreateRentalTransactionUseCase(
     prismaRentalTransactionRepository,
+    prismaTransactionHistoryRepository,
     prismaSkinRepository,
     prismaPerfilRepository,
     prismaWalletRepository,
-    prismaNotificationRepository,
-    prismaConfigurationRepository
+    prismaNotificationRepository
   );
 
   return createRentalTransaction;
---
+++ src/utils/CronJobProcessRental.ts
@@ -0,0 +1,223 @@
+import { env } from "@/env";
+
+import { ITransactionHistoryRepository } from "@/repositories/interfaceRepository/ITransactionHistoryRepository";
+import axios from "axios";
+import { IConfigurationRepository } from "@/repositories/interfaceRepository/IConfigurationRepository";
+import { TransactionHistory } from "@prisma/client";
+import { INotificationRepository } from "@/repositories/interfaceRepository/INotificationRepository";
+import { IWalletRepository } from "@/repositories/interfaceRepository/IWalletRepository";
+import { IPerfilRepository } from "@/repositories/interfaceRepository/IPerfilRepository";
+import { formatBalance } from "./formatBalance";
+import { compareDates } from "./compareDates";
+import { ISkinsRepository } from "@/repositories/interfaceRepository/ISkinsRepository";
+import { IRentalTransactionRepository } from "@/repositories/interfaceRepository/IRentalTransactionRepository";
+
+interface IUpdateTransactionHistory {
+  transactionHistory: TransactionHistory;
+}
+
+export class CronJobProcessTransaction {
+  constructor(
+    private transactionHistoryRepository: ITransactionHistoryRepository,
+    private rentalTransaction: IRentalTransactionRepository,
+    private configurationRepository: IConfigurationRepository,
+    private notificationRepository: INotificationRepository,
+    private walletRepository: IWalletRepository,
+    private perfilRepository: IPerfilRepository,
+    private skinRepository: ISkinsRepository
+  ) {}
+
+  // Pegar todas as transações, filtrar por process = false, e processar cada uma delas
+  // --------------------------------------------------------------------------------------------
+  async execute() {
+    console.log("Inciando cronjob Rental...");
+
+    // Preciso verificar o assetid, o partnersteamid e o steamcommunityapikey
+    await this.processPendingTransactions();
+
+    console.log("Cronjob Rental finalizado...");
+  }
+
+  // --------------------------------------------------------------------------------------------
+  async fetchInventory(
+    steamcommunityapikey: string,
+    partnersteamid: string,
+    assetid: string
+  ) {
+    try {
+      const baseUrl = "https://www.steamwebapi.com";
+
+      const inventorySeller = await axios
+        .post(
+          `${baseUrl}/steam/api/trade/status?key=${env.KEY_STEAM_WEB_API}`,
+          {
+            steamcommunityapikey,
+            partnersteamid,
+            assetid,
+          }
+        )
+        .then((response) => response.data);
+      return inventorySeller;
+    } catch (error) {
+      return error.response.data;
+    }
+  }
+
+  // --------------------------------------------------------------------------------------------
+  async processPendingTransactions() {
+    const allTransactions = await this.transactionHistoryRepository.findByMany(
+      false
+    );
+    if (!allTransactions.length) {
+      return "Nenhuma transação pendente.";
+    }
+
+    for (const transaction of allTransactions) {
+      const datesCompare = compareDates(transaction.dateProcess, new Date());
+      if (!datesCompare) {
+        return;
+      }
+      const inventorySeller = await this.processTransaction(transaction);
+      console.log(inventorySeller[0], "inventorySeller");
+
+      if (inventorySeller && inventorySeller.length > 0) {
+        const { completed, partnersteamid, receivedassetids, received } =
+          inventorySeller[0];
+
+        const { buyer_id, asset_id } = transaction;
+        if (
+          completed &&
+          partnersteamid === buyer_id &&
+          receivedassetids !== undefined &&
+          receivedassetids[0] === asset_id &&
+          received
+        ) {
+          await this.handleSuccessTransaction({
+            transactionHistory: transaction,
+          });
+        } else {
+          await this.handleFailedTransaction({
+            transactionHistory: transaction,
+          });
+        }
+      } else {
+        await this.handleFailedTransaction({ transactionHistory: transaction });
+      }
+    }
+    return "Transação processada com sucesso.";
+  }
+
+  // --------------------------------------------------------------------------------------------
+  async processTransaction(transaction: TransactionHistory) {
+    if (!transaction) {
+      return;
+    }
+    const config = await this.configurationRepository.findByUser(
+      transaction.seller_id
+    );
+
+    if (!config.key) {
+      return "Key steam not found";
+    }
+
+    if (config && config.key && config.key !== " ") {
+      const inventorySeller = await this.fetchInventory(
+        config.key,
+        transaction.buyer_id,
+        transaction.asset_id
+      );
+      return inventorySeller;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Logicas de sucesso e falha da transação:
+   * Se falhar, quer dizer que o comprador não devolveu a skin, então será retirado o total_price da carteira do vendedor
+   * Se de sucesso, quer dizer que o comprador devolveu a skin, então será adicionado o valor referente aos dias na carteira do vendedor
+   */
+
+  async handleSuccessTransaction({
+    transactionHistory,
+  }: IUpdateTransactionHistory) {
+    const perfilSeller = await this.perfilRepository.findByUser(
+      transactionHistory.seller_id
+    );
+    const rentalTransaction = await this.rentalTransaction.findById(
+      transactionHistory.transaction_id
+    );
+    const skin = await this.skinRepository.findById(rentalTransaction.skin_id);
+
+    await Promise.all([
+      this.perfilRepository.updateTotalExchanges(perfilSeller.id),
+
+      this.transactionHistoryRepository.updateId(transactionHistory.id, {
+        processTransaction: true,
+      }),
+      this.rentalTransaction.updateId(rentalTransaction.id, {
+        status: "Concluído",
+      }),
+      this.notificationRepository.create({
+        owner_id: transactionHistory.seller_id,
+        description: `Parabéns! O comprador realizou a devolução do item ${skin.skin_name} com sucesso. O valor ${rentalTransaction.remainder} já está na sua conta.`,
+      }),
+      this.notificationRepository.create({
+        owner_id: transactionHistory.buyer_id,
+        description: `Parabéns! Sua devolução foi finalizada com sucesso. O valor retornado foi de ${rentalTransaction.fee_total_price}.`,
+      }),
+      this.skinRepository.updateById(rentalTransaction.skin_id, {
+        status: "Concluído",
+        saledAt: new Date(),
+      }),
+      this.walletRepository.updateByUserValue(
+        transactionHistory.seller_id,
+        "increment",
+        rentalTransaction.fee_total_price
+      ),
+    ]);
+  }
+
+  async handleFailedTransaction({
+    transactionHistory,
+  }: IUpdateTransactionHistory) {
+    const perfilSeller = await this.perfilRepository.findByUser(
+      transactionHistory.seller_id
+    );
+
+    const rentalTransaction = await this.rentalTransaction.findById(
+      transactionHistory.transaction_id
+    );
+    const skin = await this.skinRepository.findById(rentalTransaction.skin_id);
+    const { formattedBalance } = formatBalance(rentalTransaction.total_price);
+
+    await Promise.all([
+      this.transactionHistoryRepository.updateId(transactionHistory.id, {
+        processTransaction: true,
+      }),
+      this.rentalTransaction.updateId(rentalTransaction.id, {
+        status: "Concluído",
+      }),
+      this.notificationRepository.create({
+        owner_id: transactionHistory.seller_id,
+        description: `A venda da skin ${skin.skin_name} foi realizada com sucesso"`,
+      }),
+      this.notificationRepository.create({
+        owner_id: transactionHistory.buyer_id,
+        description: `Você não devolveu a skin ${skin.skin_name} no prazo estipulado, por isso foi retirado o valor de ${formattedBalance} da sua carteira.`,
+      }),
+      this.walletRepository.updateByUserValue(
+        transactionHistory.seller_id,
+        "increment",
+        rentalTransaction.total_price
+      ),
+      this.skinRepository.updateById(rentalTransaction.skin_id, {
+        status: "Concluído",
+        saledAt: new Date(),
+      }),
+      this.perfilRepository.updateByUser(transactionHistory.seller_id, {
+        total_exchanges_completed: perfilSeller.total_exchanges_completed + 1,
+      }),
+    ]);
+  }
+}
