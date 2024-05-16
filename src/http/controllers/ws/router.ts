81cd23713d441870bbf004d07a79f3d0631070a5
ItaloAraujodev <italoarauju2019@gmail.com>
2024-05-16 10:18:48 -0300

    feat: add webhook route for validating pending trades

--- src/http/controllers/ws/router.ts
+++ src/http/controllers/ws/router.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { validateHistoryTradeController } from "./validateHistoryTradeController";
 
 export async function wsRouter(app: FastifyInstance) {
@@ -9,5 +9,14 @@ export async function wsRouter(app: FastifyInstance) {
   app.post(
     "/webhook/validate/send/:transactionId",
     validateHistoryTradeController
+  );
+
+  app.post(
+    "/webhook/validate/trade/pending",
+    (req: FastifyRequest, reply: FastifyReply) => {
+      const body = req.body;
+      console.log(body);
+      return reply.status(200).send({ message: body });
+    }
   );
 }
