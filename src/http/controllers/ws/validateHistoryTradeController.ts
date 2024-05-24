04f1bc305bea094970290d300d97a3991953c570
ItaloAraujodev <italoarauju2019@gmail.com>
2024-05-24 11:50:38 -0300

    refactor: test 1

--- src/http/controllers/ws/validateHistoryTradeController.ts
+++ src/http/controllers/ws/validateHistoryTradeController.ts
@@ -1,7 +1,15 @@
 import { TransactionHistoryNotExistError } from "@/useCases/@errors/TransactionHistory/TransactionHistoryNotExistError";
 import { ValidateTransactionHistoryError } from "@/useCases/@errors/ws/validateTransactionHistoryError";
-import { makeValidateTransactionHistoryUseCase } from "@/useCases/@factories/ws/makeValidateTransactionHistoryUseCase";
+import { makeCreateNotificationUseCase } from "@/useCases/@factories/Notification/makeCreateNotificationUseCase";
+import { makeGetUserPerfil } from "@/useCases/@factories/Perfil/makeGetUserPerfil";
+import { makeUpdateUserPerfil } from "@/useCases/@factories/Perfil/makeUpdatePerfil";
+import { makeUpdateSkinUseCase } from "@/useCases/@factories/Skin/makeUpdateSkinUseCase";
+import { makeGetIdTransactionUseCase } from "@/useCases/@factories/Transaction/makeGetIdTransactionUseCase";
+import { makeUpdateStatusTransactionUseCase } from "@/useCases/@factories/Transaction/makeUpdateStatusTransactionUseCase";
+import { makeGetByIdTransactionHistoryTransUseCase } from "@/useCases/@factories/TransactionHistory/makeGetByIdTransactionHistoryTransUseCase";
+import { makeUpdateWalletsValueUsersUseCase } from "@/useCases/@factories/Wallet/makeUpdateWalletsValueUsersUseCase";
 import { IGetHistoricTrade } from "@/useCases/ws/interface/getHistoricTrade";
+import { formatBalance } from "@/utils/formatBalance";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function validateHistoryTradeController(
@@ -13,11 +21,94 @@ export async function validateHistoryTradeController(
   const { historic }: { historic: IGetHistoricTrade } = req.body;
   const { transactionId } = req.params as { transactionId: string };
 
+  const makehistory = makeGetByIdTransactionHistoryTransUseCase();
+  const makeNotification = makeCreateNotificationUseCase();
+  const makeTransaction = makeGetIdTransactionUseCase();
+  const makeTransactionUpdate = makeUpdateStatusTransactionUseCase();
+  const makePerfilUser = makeGetUserPerfil();
+  const makePerfilUpdateTotal = makeUpdateUserPerfil();
+  const makeSkinUpdate = makeUpdateSkinUseCase();
+  const makeWalletUpdate = makeUpdateWalletsValueUsersUseCase();
+
+  async function handleSuccessTransaction({ transactionHistory }) {
+    console.log("Iniciou a transação");
+    const perfilSeller = await makePerfilUser.execute(
+      transactionHistory.seller_id
+    );
+
+    const transaction = await makeTransaction.execute(
+      transactionHistory.transaction_id
+    );
+    const { porcentagem } = formatBalance(transaction.balance);
+
+    await Promise.all([
+      makePerfilUpdateTotal.execute(perfilSeller.id, {
+        total_exchanges_completed: perfilSeller.total_exchanges_completed + 1,
+      }),
+
+      transactionHistory.updateId(transactionHistory.id, {
+        processTransaction: "Completed",
+      }),
+      makeTransactionUpdate.execute(transaction.id, "NegociationAccepted"),
+      makeNotification.execute({
+        owner_id: transactionHistory.seller_id,
+        description: `Parabéns! Sua venda foi finalizada com sucesso. O valor recebido foi de ${porcentagem}.`,
+      }),
+      makeNotification.execute({
+        owner_id: transactionHistory.buyer_id,
+        description: `Parabéns! Sua compra foi finalizada com sucesso.`,
+      }),
+      makeSkinUpdate.execute(transaction.skin_id, {
+        status: "Concluído",
+        saledAt: new Date(),
+      }),
+      makeWalletUpdate.execute(
+        transactionHistory.seller_id,
+        "increment",
+        porcentagem
+      ),
+    ]);
+  }
+
   try {
-    const makeValidade = makeValidateTransactionHistoryUseCase();
-    const validate = await makeValidade.execute(transactionId, historic);
-    console.log(validate);
-    return reply.status(200).send(validate);
+    const transactionHistory = await makehistory.execute(transactionId);
+    if (!transactionHistory) {
+      throw new TransactionHistoryNotExistError();
+    }
+    console.log("Passou aqui");
+    if (transactionHistory.processTransaction === "Pending") {
+      const filterTransactionParticipantsId =
+        historic.jsonPayload.payload.data.filter(
+          (item) =>
+            item.participantsteamid === transactionHistory.buyer_id &&
+            item.items.sent.length > 0
+        );
+      console.log(
+        "filterTransactionParticipantsId",
+        filterTransactionParticipantsId
+      );
+      console.log("Passou aqui 2");
+      const filterTransactionParticipantsItems = filterTransactionParticipantsId
+        .map((sents) => {
+          const filteredItems = sents.items.sent.some((item) => {
+            return item.assetid === transactionHistory.asset_id;
+          });
+          return filteredItems;
+        })
+        .filter(Boolean);
+
+      console.log(
+        "filterTransactionParticipantsItems",
+        filterTransactionParticipantsItems
+      );
+      console.log("Passou aqui 3");
+      if (filterTransactionParticipantsItems.length > 0) {
+        console.log("Passou aqui 4");
+        await handleSuccessTransaction({
+          transactionHistory,
+        });
+      }
+    }
   } catch (error) {
     if (error instanceof ValidateTransactionHistoryError) {
       return reply.status(404).send({ message: error.message });
