32e7073a56c7f2204c129af8794718dd09bcc7ea
ItaloAraujodev <italoarauju2019@gmail.com>
2024-06-05 15:21:23 -0300

    refactor: optimize getInventoryUserController

--- src/http/controllers/Skins/getInventoryUserController.ts
+++ src/http/controllers/Skins/getInventoryUserController.ts
@@ -1,7 +1,9 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { community } from "@/server";
+import { makeGetManySellerUseCase } from "@/useCases/@factories/Skin/makeGetManySellerUseCase";
+import { makeGetManyTransactionUseCase } from "@/useCases/@factories/Transaction/makeGetManyTransactionUseCase";
 
-export function getInventoryUserController(
+export async function getInventoryUserController(
   req: FastifyRequest,
   reply: FastifyReply
 ) {
@@ -17,6 +19,18 @@ export function getInventoryUserController(
   }
 
   try {
+    const makeSkins = makeGetManySellerUseCase();
+    const skinsMany = await makeSkins.execute(id, "false");
+    const transactionMany = makeGetManyTransactionUseCase();
+    const transactions = await transactionMany.execute();
+
+    if (!skinsMany) {
+      return reply.status(404).send({
+        message: "Error",
+        err: "Skins not found",
+      });
+    }
+
     return community.getUserInventoryContents(
       id,
       730,
@@ -31,7 +45,23 @@ export function getInventoryUserController(
           });
         }
 
-        return reply.status(200).send(inventory);
+        if (inventory.length > 0) {
+          const skinsManySet = new Set(
+            skinsMany.skins.map((skin) => skin.asset_id)
+          );
+          // Verificar em transactions
+          const transactionsSet = new Set(
+            transactions.map((transaction: any) => transaction.skin.asset_id)
+          );
+
+          const filterInventorySkins = inventory.filter(
+            (item) =>
+              !skinsManySet.has(item.assetid) &&
+              !transactionsSet.has(item.assetid)
+          );
+          console.log(filterInventorySkins);
+          return reply.status(200).send(filterInventorySkins);
+        }
       }
     );
   } catch (error) {
