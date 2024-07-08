a4bccafd03e34664b6c0dc27d8ed365abc741c67
ItaloAraujodev <italoarauju2019@gmail.com>
2024-07-08 18:37:31 -0300

    refactor: remove unnecessary migration files and update environment variables

--- prisma/migrations/20240613130711_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `borderColoer` on the `Skin` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Skin" DROP COLUMN "borderColoer",
-ADD COLUMN     "borderColor" VARCHAR(255);
--- prisma/migrations/20240708185108_sale_type_updat/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `sale_type` column on the `Skin` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Skin" DROP COLUMN "sale_type",
-ADD COLUMN     "sale_type" VARCHAR(255)[];
