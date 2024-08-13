962984b93ac1f3bc39d4bddf7d1984fb18ee7d6e
ItaloAraujodev <italoarauju2019@gmail.com>
2024-08-13 09:35:40 -0300

    feat: add updateStatusTransactionRentalController to rentalTransactionRouter

--- src/http/controllers/RentalTransaction/router.ts
+++ src/http/controllers/RentalTransaction/router.ts
@@ -3,12 +3,19 @@ import { verifyJwt } from "@/http/middlewares/verifyJwt";
 import { createTransactionRentalController } from "./createTransactionRentalController";
 import { getManyTransactionRentalController } from "./getManyTransactionRentalController";
 import { getManyUserTransactionRentalController } from "./getManyUserTransactionRentalController";
+import { updateStatusTransactionRentalController } from "./updateStatusTransactionRentalController";
 
 export async function rentalTransactionRouter(app: FastifyInstance) {
   app.post(
     "/v1/rentalTransaction",
     { onRequest: verifyJwt },
     createTransactionRentalController
+  );
+
+  app.patch(
+    "/v1/rentalTransaction/:id",
+    { onRequest: verifyJwt },
+    updateStatusTransactionRentalController
   );
 
   app.get("/v1/rentalTransaction", getManyTransactionRentalController);
