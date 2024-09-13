ba29404002e9c5be8317b192e8c387657efe5f17
ItaloAraujodev <italoarauju2019@gmail.com>
2024-09-13 11:38:34 -0300

    feat: history seller and buyer

--- src/useCases/wsRent/ValidateTransactionHistoryReturnBuyerTransactionRentalUseCase.ts
+++ src/useCases/wsRent/ValidateTransactionHistoryReturnBuyerTransactionRentalUseCase.ts
@@ -8,24 +8,25 @@ import {
   RentalTransaction as PrismaRentalTransaction,
   Skin,
 } from "@prisma/client";
+import type { IWalletRepository } from "@/repositories/interfaceRepository/IWalletRepository";
 
 interface RentalTransaction extends PrismaRentalTransaction {
   skinsRent?: Skin[];
 }
 
-export class ValidateTransactionHistoryRentalUseCase {
+export class ValidateTransactionHistoryReturnBuyerTransactionRentalUseCase {
   constructor(
     private transactionRentalRepository: IRentalTransactionRepository,
     private notificationRepository: INotificationRepository,
     private perfilRepository: IPerfilRepository,
-    private skinRepository: ISkinsRepository
+    private skinRepository: ISkinsRepository,
+    private walletRepository: IWalletRepository
   ) {}
 
   async execute(
     transactionId: string,
     historic: IGetHistoricTrade
   ): Promise<void | string> {
-    let dateStart: string;
     const transactionRental: RentalTransaction =
       await this.transactionRentalRepository.findById(transactionId);
 
@@ -33,8 +34,10 @@ export class ValidateTransactionHistoryRentalUseCase {
       throw new TransactionNotExistError();
     }
     console.log((historic as any).data);
-    console.log("Iniciando!");
-    if (transactionRental.status === "WaitingForSellerConfirmation") {
+    console.log("Iniciando! Buyer");
+    if (
+      transactionRental.status === "WaitingForAdministratorsReturnConfirmation"
+    ) {
       const filterTransactionParticipantsId = historic.payload.data.filter(
         (item) => {
           return (
@@ -46,7 +49,6 @@ export class ValidateTransactionHistoryRentalUseCase {
 
       const matchingItems = filterTransactionParticipantsId.some(
         (offer: Daum) => {
-          dateStart = offer.date;
           return transactionRental.skinsRent.every((skin) => {
             return offer.items.sent.some((item) => {
               return (
@@ -59,11 +61,14 @@ export class ValidateTransactionHistoryRentalUseCase {
         }
       );
 
-      console.log("filterTransactionParticipantsItems", matchingItems);
+      console.log(
+        "filterTransactionParticipantsItems ------- BUYER",
+        matchingItems
+      );
 
       if (matchingItems) {
-        console.log("Entrou aqui 2");
-        await this.handleSuccessTransaction(transactionRental, dateStart);
+        console.log("Entrou aqui 2 --- BUYER");
+        await this.handleSuccessTransaction(transactionRental);
         return "Transação concluída com sucesso";
       }
       return "Nada a ser feito";
@@ -71,38 +76,43 @@ export class ValidateTransactionHistoryRentalUseCase {
   }
 
   async handleSuccessTransaction(
-    transactionRental: RentalTransaction,
-    dateStart: string
+    transactionRental: RentalTransaction
   ): Promise<void> {
+    const commissionRate = 0.1; // 10% de comissão
     try {
-      // const perfilSeller = await this.perfilRepository.findByUser(
-      //   transactionRental.skinsRent![0].seller_id
-      // );
+      const perfilSeller = await this.perfilRepository.findByUser(
+        transactionRental.skinsRent![0].seller_id
+      );
 
       const skinIds = transactionRental.skinsRent!.map((skin) => skin.id);
 
-      const startDate = new Date(dateStart);
-      const daysToAdd = Math.max(transactionRental.daysQuantity, 7);
-      const endDateNew = new Date(startDate);
-      endDateNew.setDate(endDateNew.getDate() + daysToAdd);
+      const totalSkinPrice = transactionRental.skinsRent.reduce(
+        (total, skin) => total + skin.skin_price,
+        0
+      );
+      const sellerId = transactionRental.skinsRent[0].seller_id;
+      const sellerAmount = totalSkinPrice * (1 - commissionRate);
 
       await Promise.all([
+        this.perfilRepository.updateTotalExchanges((perfilSeller as any).id),
         this.transactionRentalRepository.updateId(transactionRental.id, {
-          status: "TrialPeriodStarted",
-          startDate,
-          endDate: endDateNew,
-          sellerConfirmedAt: new Date(),
-          trialPeriodStartedAt: new Date(),
+          status: "Completed",
+          returnDueAt: new Date(),
         }),
         this.skinRepository.updateMany(skinIds, "Completed"),
         this.notificationRepository.create({
           owner_id: transactionRental.skinsRent![0].seller_id,
-          description: `Parabéns! A partir de agora o locatário iniciará o período de teste da skin.`,
+          description: `Parabéns! A locação da skin foi concluída com sucesso. O valor de ${sellerAmount} foi adicionado à sua carteira.`,
         }),
         this.notificationRepository.create({
           owner_id: transactionRental.buyerId,
-          description: `Parabéns! A partir de agora você iniciará o período de teste da skin.`,
+          description: `Parabéns! A locação da skin foi concluída com sucesso.`,
         }),
+        this.walletRepository.updateByUserValue(
+          sellerId,
+          "increment",
+          sellerAmount
+        ),
       ]);
     } catch (error) {
       console.error("Erro na handleSuccessTransaction", error);
--- src/useCases/wsRent/validateTransactionHistoryReturnSellerTransactionRentalUseCase.ts
+++ src/useCases/wsRent/validateTransactionHistoryReturnSellerTransactionRentalUseCase.ts
@@ -8,24 +8,25 @@ import {
   RentalTransaction as PrismaRentalTransaction,
   Skin,
 } from "@prisma/client";
+import type { IWalletRepository } from "@/repositories/interfaceRepository/IWalletRepository";
 
 interface RentalTransaction extends PrismaRentalTransaction {
   skinsRent?: Skin[];
 }
 
-export class ValidateTransactionHistoryRentalUseCase {
+export class ValidateTransactionHistoryReturnSellerTransactionRentalUseCase {
   constructor(
     private transactionRentalRepository: IRentalTransactionRepository,
     private notificationRepository: INotificationRepository,
     private perfilRepository: IPerfilRepository,
-    private skinRepository: ISkinsRepository
+    private skinRepository: ISkinsRepository,
+    private walletRepository: IWalletRepository
   ) {}
 
   async execute(
     transactionId: string,
     historic: IGetHistoricTrade
   ): Promise<void | string> {
-    let dateStart: string;
     const transactionRental: RentalTransaction =
       await this.transactionRentalRepository.findById(transactionId);
 
@@ -33,22 +34,23 @@ export class ValidateTransactionHistoryRentalUseCase {
       throw new TransactionNotExistError();
     }
     console.log((historic as any).data);
-    console.log("Iniciando!");
-    if (transactionRental.status === "WaitingForSellerConfirmation") {
+    console.log("Iniciando! ------ Seller");
+    if (
+      transactionRental.status === "WaitingForAdministratorsReturnConfirmation"
+    ) {
       const filterTransactionParticipantsId = historic.payload.data.filter(
         (item) => {
           return (
             item.participantsteamid === transactionRental.buyerId &&
-            item.items.sent.length > 0
+            item.items.received.length > 0
           );
         }
       );
 
       const matchingItems = filterTransactionParticipantsId.some(
         (offer: Daum) => {
-          dateStart = offer.date;
           return transactionRental.skinsRent.every((skin) => {
-            return offer.items.sent.some((item) => {
+            return offer.items.received.some((item) => {
               return (
                 item.markethashname === skin.skin_market_hash_name &&
                 item.instanceid === skin.skin_instanceid &&
@@ -59,11 +61,14 @@ export class ValidateTransactionHistoryRentalUseCase {
         }
       );
 
-      console.log("filterTransactionParticipantsItems", matchingItems);
+      console.log(
+        "filterTransactionParticipantsItems -------- SELLER",
+        matchingItems
+      );
 
       if (matchingItems) {
-        console.log("Entrou aqui 2");
-        await this.handleSuccessTransaction(transactionRental, dateStart);
+        console.log("Entrou aqui 2 ---- SELLER");
+        await this.handleSuccessTransaction(transactionRental);
         return "Transação concluída com sucesso";
       }
       return "Nada a ser feito";
@@ -71,38 +76,43 @@ export class ValidateTransactionHistoryRentalUseCase {
   }
 
   async handleSuccessTransaction(
-    transactionRental: RentalTransaction,
-    dateStart: string
+    transactionRental: RentalTransaction
   ): Promise<void> {
+    const commissionRate = 0.1; // 10% de comissão
     try {
-      // const perfilSeller = await this.perfilRepository.findByUser(
-      //   transactionRental.skinsRent![0].seller_id
-      // );
+      const perfilSeller = await this.perfilRepository.findByUser(
+        transactionRental.skinsRent![0].seller_id
+      );
 
       const skinIds = transactionRental.skinsRent!.map((skin) => skin.id);
 
-      const startDate = new Date(dateStart);
-      const daysToAdd = Math.max(transactionRental.daysQuantity, 7);
-      const endDateNew = new Date(startDate);
-      endDateNew.setDate(endDateNew.getDate() + daysToAdd);
+      const totalSkinPrice = transactionRental.skinsRent.reduce(
+        (total, skin) => total + skin.skin_price,
+        0
+      );
+      const sellerId = transactionRental.skinsRent[0].seller_id;
+      const sellerAmount = totalSkinPrice * (1 - commissionRate);
 
       await Promise.all([
+        this.perfilRepository.updateTotalExchanges((perfilSeller as any).id),
         this.transactionRentalRepository.updateId(transactionRental.id, {
-          status: "TrialPeriodStarted",
-          startDate,
-          endDate: endDateNew,
-          sellerConfirmedAt: new Date(),
-          trialPeriodStartedAt: new Date(),
+          status: "Completed",
+          returnDueAt: new Date(),
         }),
         this.skinRepository.updateMany(skinIds, "Completed"),
         this.notificationRepository.create({
           owner_id: transactionRental.skinsRent![0].seller_id,
-          description: `Parabéns! A partir de agora o locatário iniciará o período de teste da skin.`,
+          description: `Parabéns! A locação da skin foi concluída com sucesso. O valor de ${sellerAmount} foi adicionado à sua carteira.`,
         }),
         this.notificationRepository.create({
           owner_id: transactionRental.buyerId,
-          description: `Parabéns! A partir de agora você iniciará o período de teste da skin.`,
+          description: `Parabéns! A locação da skin foi concluída com sucesso.`,
         }),
+        this.walletRepository.updateByUserValue(
+          sellerId,
+          "increment",
+          sellerAmount
+        ),
       ]);
     } catch (error) {
       console.error("Erro na handleSuccessTransaction", error);
