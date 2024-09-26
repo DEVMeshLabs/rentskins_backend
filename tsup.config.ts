9473ecafccbf5fc9edde25eb54343be5d5e7b875
ItaloAraujodev <italoarauju2019@gmail.com>
2024-09-26 12:20:37 -0300

    refactor test 3

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig } from "tsup";
-import tsconfigPaths from "tsconfig-paths";
-
-tsconfigPaths.register(); // Registra os aliases do tsconfig
 
 export default defineConfig({
   entry: ["src", "!src/tests"],
