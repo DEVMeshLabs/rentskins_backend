ab660fa0108cc08036f80dd4a4e5f2d4f2029501
ItaloAraujodev <italoarauju2019@gmail.com>
2024-10-05 14:24:26 -0300

    test 2

--- src/useCases/ws/validatePendingTradeReceived.ts
+++ src/useCases/ws/validatePendingTradeReceived.ts
@@ -1,38 +1,137 @@
-import { TransactionHistoryNotExistError } from "../@errors/TransactionHistory/TransactionHistoryNotExistError";
-import { StatusHasAlreadyBeenUpdatedError } from "../@errors/ws/StatusHasAlreadyBeenUpdatedError";
-import { IGetTradesPending } from "./interface/getTradesPending";
+import { ITransactionHistoryRepository } from "@/repositories/interfaceRepository/ITransactionHistoryRepository";
+import { Daum, IGetHistoricTrade } from "./interface/getHistoricTrade";
+import { IConfigurationRepository } from "@/repositories/interfaceRepository/IConfigurationRepository";
+import { INotificationRepository } from "@/repositories/interfaceRepository/INotificationRepository";
+import { IPerfilRepository } from "@/repositories/interfaceRepository/IPerfilRepository";
 import { ISkinsRepository } from "@/repositories/interfaceRepository/ISkinsRepository";
+import { IWalletRepository } from "@/repositories/interfaceRepository/IWalletRepository";
+import { TransactionHistory } from "@prisma/client";
+import { formatBalance } from "@/utils/formatBalance";
 import { ITransactionRepository } from "@/repositories/interfaceRepository/ITransactionRepository";
+import { TransactionHistoryNotExistError } from "../@errors/TransactionHistory/TransactionHistoryNotExistError";
 
-export class ValidatePendingTradeReceived {
+interface IUpdateTransactionHistory {
+  transactionHistory: TransactionHistory;
+}
+
+export class ValidateTransactionHistoryUseCase {
   constructor(
+    private transactionHistory: ITransactionHistoryRepository,
     private transactionRepository: ITransactionRepository,
+    private configurationRepository: IConfigurationRepository,
+    private notificationRepository: INotificationRepository,
+    private walletRepository: IWalletRepository,
+    private perfilRepository: IPerfilRepository,
     private skinRepository: ISkinsRepository
   ) {}
 
   async execute(
     transactionId: string,
-    historic: IGetTradesPending
-  ): Promise<any> {
-    const transaction = await this.transactionRepository.findById(
+    historic: IGetHistoricTrade
+  ): Promise<void | string> {
+    const transactionHistory = await this.transactionHistory.findByTrasactionId(
       transactionId
     );
 
-    if (!transaction) {
+    if (!transactionHistory) {
       throw new TransactionHistoryNotExistError();
-    } else if (transaction.status === "NegotiationSend") {
-      throw new StatusHasAlreadyBeenUpdatedError();
     }
-    if (transaction.status === "Default") {
-      // const tradeoffers = historic.jsonPayload.payload.tradeoffers;
+    const transaction = await this.transactionRepository.findById(
+      transactionHistory.transaction_id
+    );
+    if (transactionHistory.processTransaction === "Pending") {
+      const skin = await this.skinRepository.findById(transaction.skin_id);
 
-      const response = await this.transactionRepository.updateStatus(
-        transactionId,
-        "NegotiationSend"
+      const filterTransactionParticipantsId = historic.payload.data.filter(
+        (item) => {
+          return (
+            item.participantsteamid === transactionHistory.buyer_id &&
+            item.items.sent.length > 0
+          );
+        }
       );
-      return response;
+      console.log(
+        "filterTransactionParticipantsId",
+        filterTransactionParticipantsId
+      );
+      const filterTransactionParticipantsItems =
+        filterTransactionParticipantsId.filter((tran: Daum) => {
+          return tran.items.sent.some((item) => {
+            console.log("Items", item);
+            return (
+              item.markethashname === skin.skin_market_hash_name &&
+              item.instanceid === skin.skin_instanceid &&
+              item.classid === skin.skin_classid
+            );
+          });
+        });
+      console.log(
+        "filterTransactionParticipantsItems",
+        filterTransactionParticipantsItems
+      );
+      if (filterTransactionParticipantsItems.length) {
+        console.log("Entrou aqui 2");
+        await this.handleSuccessTransaction({
+          transactionHistory,
+        });
+        return "Transação concluída com sucesso";
+      }
+      return "Nada a ser feito";
     }
   }
 
-  async handleSuccessTransaction(id: string) {}
+  async handleSuccessTransaction({
+    transactionHistory,
+  }: IUpdateTransactionHistory) {
+    try {
+      const perfilSeller = await this.perfilRepository.findByUser(
+        transactionHistory.seller_id[0]
+      );
+
+      if (!perfilSeller) {
+        throw new Error("Seller profile not found.");
+      }
+
+      const transaction = await this.transactionRepository.findById(
+        transactionHistory.transaction_id
+      );
+      if (!transaction) {
+        throw new Error("Transaction not found.");
+      }
+
+      const { porcentagem } = formatBalance(transaction.balance);
+
+      await Promise.all([
+        this.perfilRepository.updateTotalExchanges(perfilSeller.id as any),
+        this.transactionHistory.updateId(transactionHistory.id, {
+          processTransaction: "Completed",
+        }),
+        this.transactionRepository.updateStatus(
+          transaction.id,
+          "NegociationAccepted"
+        ),
+        this.notificationRepository.create({
+          owner_id: transactionHistory.seller_id[0],
+          description: `Parabéns! Sua venda foi finalizada com sucesso. O valor recebido foi de ${porcentagem}.`,
+        }),
+        this.notificationRepository.create({
+          owner_id: transactionHistory.buyer_id,
+          description: `Parabéns! Sua compra foi finalizada com sucesso.`,
+        }),
+        this.skinRepository.updateById(transaction.skin_id, {
+          status: "Concluído",
+          saledAt: new Date(),
+        }),
+        this.walletRepository.updateByUserValue(
+          transactionHistory.seller_id[0],
+          "increment",
+          porcentagem
+        ),
+      ]);
+    } catch (error) {
+      console.error("Error handling success transaction:", error);
+      // Aqui você pode adicionar lógica para registrar o erro ou retornar uma resposta adequada
+      throw error; // Re-throw the error if you want it to propagate
+    }
+  }
 }
